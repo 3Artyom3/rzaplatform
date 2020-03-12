675c2bc5c56e29119d1182e6f47a29dfca039df5
Artyom <eoda@mail.ru>
2020-03-12 19:56:23 +0300

    TextComponent Artyom

---
+++ src/components/TextComponent.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './ExploreContainer.css';
+
+interface Props {
+  name: string;
+}
+
+const TextComponent: React.FC<Props> = ({ name }) => {
+  return (
+    <div className="container">
+      <text> Приветб {name} </text>    
+    </div>
+  );
+};
+
+export default TextComponent;
